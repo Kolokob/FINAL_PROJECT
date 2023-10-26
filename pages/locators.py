4815ff369966043feebab159c66dc641e6a74cd5
Kolokoob <mykytatakhtamyshev@gmail.com>
2023-10-25 18:28:51 -0700

    Added locators.py, login_page.py Modified main_page.py, test_main_page.py

---
+++ pages/locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.XPATH, "//a[@id='registration_link']")
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.XPATH, "//*[@id='login_form']")
+    REGISTRATION_FORM = (By.XPATH, "//*[@id='register_form']")
